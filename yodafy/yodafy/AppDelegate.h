bf64b5fdfb39592c460f490efcbe931a5aa94627
Colin Smith <colinsmith@Colins-MacBook-Pro.local>
2015-10-15 18:25:53 -0400

    initial project commit

---
+++ yodafy/yodafy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yodafy
+//
+//  Created by Colin Smith on 10/15/15.
+//  Copyright © 2015 Colin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
